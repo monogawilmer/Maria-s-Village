748dd82adad2007d9cbd7dfb05cfa03bd300ffaa
PapiCarryFabs <fabiandresbarreto@gmail.com>
2019-06-17 23:30:43 -0500

    Menu

---
+++ Maria's Village/Assets/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Panel;
+    private bool paused = false;
+
+    void Start()
+    {
+        Panel.SetActive(false);   
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+        }
+
+        if (paused)
+        {
+            Panel.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (!paused)
+        {
+            Panel.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
