5ba4726f67d8d7d8eddf5059bdd0b27d3abc78c0
Andres Felipe Herrera Jerez <33936503+Andresxv@users.noreply.github.com>
2019-06-17 19:24:01 -0500

    gg

---
+++ Maria's Village/Assets/Scripts/glasses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class glasses : MonoBehaviour
+{
+    public Transform from;
+    public Transform target;
+    public float speed;
+    public Transform to;
+    private Vector3 start, end;
+
+    // Start is called before the first frame update
+    void onDrawGizmosSelected()
+    {
+        Gizmos.color= Color.cyan;
+        Gizmos.DrawLine(@from.position,to.position);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        target.parent = null;
+        start = transform.position;
+        end = target.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float fixedSpeed = speed * Time.deltaTime;
+        transform.position=Vector3.MoveTowards(transform.position, target.position,fixedSpeed);
+        if (target.position == transform.position)
+        {
+            target.position = (target.position == start) ? end : start;
+        }
+    }
+}
